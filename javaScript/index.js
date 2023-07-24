1c136367b75c51a0d58e016e2f7278f0f8654a54
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-07-25 03:13:43 +0600

    Apply the js code only for mobile

--- javaScript/index.js
+++ javaScript/index.js
@@ -5,27 +5,28 @@ const menuLink2 = document.getElementById("menu-link2");
 const menuLink3 = document.getElementById("menu-link3");
 const navBar = document.getElementById("navbar");
 
-openMenu.onclick = revelMenu;
-closeMenu.onclick = removeMenu;
+function checkDisplayWidth() {
+  openMenu.onclick = revelMenu;
+  closeMenu.onclick = removeMenu;
 
-function revelMenu() {
-  navBar.style.left = "0";
-}
-
-function removeMenu() {
-  navBar.style.left = "100%";
-}
-
-document.onclick = function moveMenu(event) {
-  switch (event.target) {
-    case menuLink1:
+  if (window.innerWidth < 768) {
+    function revelMenu() {
+      navBar.style.left = "0";
+    }
+    
+    function removeMenu() {
       navBar.style.left = "100%";
-      break;
-    case menuLink2:
-      navBar.style.left = "100%";
-      break;
-    case menuLink3:
-      navBar.style.left = "100%";
-      break;
+    }
+    
+    document.onclick = function moveMenu(event) {
+      switch (event.target) {
+        case menuLink1:
+        case menuLink2:
+        case menuLink3:
+          navBar.style.left = "100%";
+          break;
+        default:
+      }
+    }
   }
 }
