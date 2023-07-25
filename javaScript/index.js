3cf12bf8028dc405f7c934ac0178973a1bcdc48f
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-07-25 14:06:40 +0600

    Mobile menu created

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,32 +1,44 @@
-const openMenu = document.getElementById("open-menu");
-const closeMenu = document.getElementById("close-menu");
-const menuLink1 = document.getElementById("menu-link1");
-const menuLink2 = document.getElementById("menu-link2");
-const menuLink3 = document.getElementById("menu-link3");
-const navBar = document.getElementById("navbar");
+function applyMobileBehavior() {
+  const openMenu = document.getElementById("open-menu");
+  const closeMenu = document.getElementById("close-menu");
+  const menuLink1 = document.getElementById("menu-link1");
+  const menuLink2 = document.getElementById("menu-link2");
+  const menuLink3 = document.getElementById("menu-link3");
+  const navBar = document.getElementById("navbar");
 
-function checkDisplayWidth() {
-  openMenu.onclick = revelMenu;
+  openMenu.onclick = revealMenu;
   closeMenu.onclick = removeMenu;
 
-  if (window.innerWidth < 768) {
-    function revelMenu() {
+  function revealMenu() {
       navBar.style.left = "0";
-    }
-    
-    function removeMenu() {
+  }
+
+  function removeMenu() {
       navBar.style.left = "100%";
-    }
-    
-    document.onclick = function moveMenu(event) {
+  }
+
+  document.onclick = function moveMenu(event) {
       switch (event.target) {
-        case menuLink1:
-        case menuLink2:
-        case menuLink3:
-          navBar.style.left = "100%";
-          break;
-        default:
+          case menuLink1:
+          case menuLink2:
+          case menuLink3:
+              navBar.style.left = "100%";
+              break;
+          default:
       }
-    }
   }
 }
+
+const mobileQuery = window.matchMedia("(max-width: 768px)");
+
+function handleScreenChange(event) {
+    if (event.matches) {
+        applyMobileBehavior();
+    } else {
+      document.getElementById("navbar").style.left = "0";
+    }
+}
+
+handleScreenChange(mobileQuery);
+
+mobileQuery.addEventListener('change', handleScreenChange);
