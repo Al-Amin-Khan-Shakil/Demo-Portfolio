9b0b4ba5ecfbc469421d3e833e2a8ccc4f9fa733
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-07-25 14:31:50 +0600

    Solved linter errors

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,42 +1,42 @@
 function applyMobileBehavior() {
-  const openMenu = document.getElementById("open-menu");
-  const closeMenu = document.getElementById("close-menu");
-  const menuLink1 = document.getElementById("menu-link1");
-  const menuLink2 = document.getElementById("menu-link2");
-  const menuLink3 = document.getElementById("menu-link3");
-  const navBar = document.getElementById("navbar");
+  const openMenu = document.getElementById('open-menu');
+  const closeMenu = document.getElementById('close-menu');
+  const menuLink1 = document.getElementById('menu-link1');
+  const menuLink2 = document.getElementById('menu-link2');
+  const menuLink3 = document.getElementById('menu-link3');
+  const navBar = document.getElementById('navbar');
+
+  function revealMenu() {
+    navBar.style.left = '0';
+  }
+
+  function removeMenu() {
+    navBar.style.left = '100%';
+  }
 
   openMenu.onclick = revealMenu;
   closeMenu.onclick = removeMenu;
 
-  function revealMenu() {
-      navBar.style.left = "0";
-  }
-
-  function removeMenu() {
-      navBar.style.left = "100%";
-  }
-
   document.onclick = function moveMenu(event) {
-      switch (event.target) {
-          case menuLink1:
-          case menuLink2:
-          case menuLink3:
-              navBar.style.left = "100%";
-              break;
-          default:
-      }
-  }
+    switch (event.target) {
+      case menuLink1:
+      case menuLink2:
+      case menuLink3:
+        navBar.style.left = '100%';
+        break;
+      default:
+    }
+  };
 }
 
-const mobileQuery = window.matchMedia("(max-width: 768px)");
+const mobileQuery = window.matchMedia('(max-width: 768px)');
 
 function handleScreenChange(event) {
-    if (event.matches) {
-        applyMobileBehavior();
-    } else {
-      document.getElementById("navbar").style.left = "0";
-    }
+  if (event.matches) {
+    applyMobileBehavior();
+  } else {
+    document.getElementById('navbar').style.left = '0';
+  }
 }
 
 handleScreenChange(mobileQuery);
