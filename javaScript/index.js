6397d29b8f3ec65b1144f4ebc8edc0a7903af4f4
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-07-28 00:31:01 +0600

    Dynamic popup is created

--- javaScript/index.js
+++ javaScript/index.js
@@ -116,85 +116,87 @@ for (let i = 0; i < projects.length; i += 1) {
 }
 
 const modalContainer = document.getElementById('popup');
-    
-for (let i = 0; i < projects.length; i += 1) {
-  let fetch = modalContainer.innerHTML;
-
-  modalContainer.innerHTML = `
-  <div id="modal">
-      <div class="topsection">
-        <div class="offBTN" id="popup-offbtn">&times;</div>
-        <div class="bgPhoto"></div>
-      </div>
-      <div class="bottomsection">
-        <div class="components">
-          <div class="compo-title">
-              <h3 class="co-title">
-                  ${projects[i].popupTitle}
-              </h3>
-          </div>
-          <div class="compo-links">
-            <div class="moblink">
-              <a href="#">${projects[i].technologies[0]}</a>
-              <a href="#">${projects[i].technologies[1]}</a>
-              <a href="#">${projects[i].technologies[2]}</a>
-            </div>
-            <div class="desklink">
-              <a href="#">${projects[i].popupTech[0]}</a>
-              <a href="#">${projects[i].popupTech[1]}</a>
-              <a href="#">${projects[i].technologies[2]}</a>
-              <a href="#">${projects[i].popupTech[2]}</a>
-              <a href="#">${projects[i].popupTech[3]}</a>
-              <a href="#">${projects[i].popupTech[4]}</a>
-            </div>
-          </div>
-        </div>
-        <div class="pragraph">
-          <p>${projects[i].popupDes}</p>
-          <p>${projects[i].popupDes}</p>
-        </div>
-        <div class="modalbtn">
-          <a href="#">See live <i class="fa fa-external-link"> </i></a>
-          <a href="#">See source <i class="fa fa-github"></i> </a>
-        </div>
-      </div>
-    </div>`
-}
-
 const popupOn1 = document.getElementById('popupbtn1');
 const popupOn2 = document.getElementById('popupbtn2');
 const popupOn3 = document.getElementById('popupbtn3');
 const popupOn4 = document.getElementById('popupbtn4');
 const popupOn5 = document.getElementById('popupbtn5');
 const popupOn6 = document.getElementById('popupbtn6');
-const popupOff = document.getElementById('popup-offbtn');
 const popUp = document.getElementById('popup-wrapper');
+const popupOn = [popupOn1, popupOn2, popupOn3, popupOn4, popupOn5, popupOn6];
 
 function openModal() {
   popUp.style.display = 'block';
 }
 
-function closeModal() {
-  popUp.style.display = 'none';
-}
-
-function showWebsite(event) {
-  if (event.target === modalContainer) {
-    popUp.style.display = 'none';
-  }
-}
-
-document.addEventListener('click', showWebsite);
-popupOff.onclick = closeModal;
-modalContainer.onclick = showWebsite;
 document.addEventListener('click', (event) => {
   switch (event.target) {
-    case popupOn1:
-    case popupOn2:
-    case popupOn3:
-    case popupOn4:
-    case popupOn5:
-    case popupOn6:
+    case popupOn[0]:
+    case popupOn[1]:
+    case popupOn[2]:
+    case popupOn[3]:
+    case popupOn[4]:
+    case popupOn[5]:
+      let index = popupOn.indexOf(event.target);
+      function closeModal() {
+        popUp.style.display = 'none';
+      }
+      
+      function showWebsite(event) {
+        if (event.target === modalContainer) {
+          popUp.style.display = 'none';
+        }
+      }
+      
+      for (let i = 0; i < popupOn.length; i += 1) {
+      
+        modalContainer.innerHTML = `
+          <div id="modal">
+            <div class="topsection">
+              <div class="offBTN" id="popup-offbtn">&times;</div>
+              <div class="bgPhoto"></div>
+            </div>
+            <div class="bottomsection">
+              <div class="components">
+                <div class="compo-title">
+                    <h3 class="co-title">
+                        ${projects[index].popupTitle}
+                    </h3>
+                </div>
+                <div class="compo-links">
+                  <div class="moblink">
+                    <a href="#">${projects[index].technologies[0]}</a>
+                    <a href="#">${projects[index].technologies[1]}</a>
+                    <a href="#">${projects[index].technologies[2]}</a>
+                  </div>
+                  <div class="desklink">
+                    <a href="#">${projects[index].popupTech[0]}</a>
+                    <a href="#">${projects[index].popupTech[1]}</a>
+                    <a href="#">${projects[index].technologies[2]}</a>
+                    <a href="#">${projects[index].popupTech[2]}</a>
+                    <a href="#">${projects[index].popupTech[3]}</a>
+                    <a href="#">${projects[index].popupTech[4]}</a>
+                  </div>
+                </div>
+              </div>
+              <div class="pragraph">
+                <p>${projects[i].popupDes}</p>
+                <p>${projects[i].popupDes}</p>
+              </div>
+              <div class="modalbtn">
+                <a href="#">See live <i class="fa fa-external-link"> </i></a>
+                <a href="#">See source <i class="fa fa-github"></i> </a>
+              </div>
+            </div>
+          </div>`
+      }
+      
+      const popupOff = document.getElementById('popup-offbtn');
+      
+      document.addEventListener('click', showWebsite);
+      popupOff.onclick = closeModal;
+      modalContainer.onclick = showWebsite;
+      
       openModal();
       break;
     default:
